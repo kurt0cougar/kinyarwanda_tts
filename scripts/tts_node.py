8f624561beb1311cfbaa0ad4d488c77aa26e778f
kurt0cougar <kkleber000@gmail.com>
2024-08-11 03:43:15 +0200

    Update tts_node.py

--- scripts/tts_node.py
+++ scripts/tts_node.py
@@ -6,19 +6,24 @@ import tempfile
 from TTS.utils.synthesizer import Synthesizer
 import subprocess
 import os
+import rospkg
+
+rospack = rospkg.RosPack()
+path = rospack.get_path('kinyarwanda_tts')
+model_path = os.path.join(path,"model_files")
+print("Path to package:", path)
+
 python2_path = '/usr/bin/python2'
-python2_script = os.path.join("/home/kk/tts_ws/src/kinyarwanda_tts/scripts",'send_and_play_audio.py')
+python2_script = os.path.join(path,"scripts",'send_and_play_audio.py')
 class KinyarwandaTTSNode:
     def __init__(self):
         rospy.init_node('kinyarwandaTTS')
         
         # Initialize Coqui TTS model (example, adjust as per your model)
         
-        self.synthesizer = Synthesizer("/home/kk/Desktop/model.pth",
-            "/home/kk/Desktop/config.json",
-            tts_speakers_file="/home/kk/Desktop/speakers.pth",
-            encoder_checkpoint="/home/kk/Desktop/SE_checkpoint.pth.tar",
-            encoder_config="/home/kk/Desktop/config_se.json",use_cuda=False)
+        self.synthesizer = Synthesizer(os.path.join(model_path,"model.pth"),
+            os.path.join(model_path,"config.json"),
+            tts_speakers_file=os.path.join(model_path,"speakers.pth"))
         # ROS Subscribers and Publishers
         rospy.Subscriber('text_to_say', String, self.text_to_say_callback)
         
@@ -28,7 +33,7 @@ class KinyarwandaTTSNode:
         
         # Generate speech from text
         #audio_data = self.tts(text)
-        wav = self.synthesizer.tts(text, speaker_wav="/home/kk/Desktop/conditioning_audio.wav")
+        wav = self.synthesizer.tts(text, speaker_wav=os.path.join(model_path,"conditioning_audio.wav"))
         # Publish audio data (you need to implement this part based on your needs)
         # For simplicity, you can publish audio as a ROS topic or save to a file
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as fp:
